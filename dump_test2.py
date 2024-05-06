c2688ab4f48abc2266ff967b367fe3f45f156eb2
Miguel <mtorr164@ucr.edu>
2024-05-05 18:31:21 -0700

    added scheduler to crawler

--- dump_test2.py
+++ dump_test2.py
@@ -8,6 +8,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import sys
+import schedule
 
 #Testing this code so that it stores the data after each post so that when it crashes, not everything is lost
 #also trying to see if i can make it so that running it multiple times will get new uniquen posts
@@ -48,66 +49,77 @@ def find_html_links(text):
     return re.findall(r'https?://\S+', text)
 
 #array to store different subreddits
-subreddits = ["gaming", "wow", "pcmasterrace", "Genshin_Impact", "nintendo", "pokemon"]
-for subreddit in subreddits:
-    output_file_path = f'hot_new_posts/{subreddit}.json'
-    with open(output_file_path, 'w', encoding='utf-8') as json_file:
-        json_file.write('[')
-        first_post = True
+def crawl_and_store():    
+    subreddits = ["gaming", "wow", "pcmasterrace", "Genshin_Impact", "nintendo", "pokemon"]
+    for subreddit in subreddits:
+        output_file_path = f'hot_new_posts/{subreddit}.json'
+        with open(output_file_path, 'w', encoding='utf-8') as json_file:
+            json_file.write('[')
+            first_post = True
 
-        for submission_set in ['hot', 'new', 'top']: #crawls hot, then new, then top with no limit
-            new_count = 1;
-            if submission_set == 'top':
-                post_limit = 120  #used to adjust top post limits
-                posts = getattr(reddit.subreddit(subreddit), submission_set)(limit=post_limit)  # Fetch top posts from all time
-            else:
-                posts = getattr(reddit.subreddit(subreddit), submission_set)(limit=None)  # Fetch hot and new posts, limit can be adjusted
+            for submission_set in ['hot', 'new', 'top']: #crawls hot, then new, then top with no limit
+                new_count = 1;
+                if submission_set == 'top':
+                    post_limit = 120  #used to adjust top post limits
+                    posts = getattr(reddit.subreddit(subreddit), submission_set)(limit=post_limit)  # Fetch top posts from all time
+                else:
+                    posts = getattr(reddit.subreddit(subreddit), submission_set)(limit=None)  # Fetch hot and new posts, limit can be adjusted
 
-            for post in posts:
-                if post.id not in seen_ids:
-                    seen_ids.add(post.id)
+                for post in posts:
+                    if post.id not in seen_ids:
+                        seen_ids.add(post.id)
 
-                    print(f"Currently on {submission_set} post {new_count} in {subreddit}\n") #output statement to show progress ofm crawler
-                    new_count += 1
+                        print(f"Currently on {submission_set} post {new_count} in {subreddit}\n") #output statement to show progress ofm crawler
+                        new_count += 1
 
-                    #check text for urls and save if exist
-                    html_links = find_html_links(post.selftext)
-                    linked_html_titles = {url: get_html_title(url) for url in html_links}
+                        #check text for urls and save if exist
+                        html_links = find_html_links(post.selftext)
+                        linked_html_titles = {url: get_html_title(url) for url in html_links}
 
-                    try:
-                        post.comments.replace_more(limit=450) #adjust comments here
-                    except prawcore.exceptions.TooManyRequests:
-                        print(f"{subreddit}'s rate limit exceeded. \n")
-                        time.sleep(60)
-                        continue
-                    except prawcore.exceptions.PRAWException as e:
-                        print(f"An error occurred while expanding comments \n")
-                        continue
+                        try:
+                            post.comments.replace_more(limit=450) #adjust comments here
+                        except prawcore.exceptions.TooManyRequests:
+                            print(f"{subreddit}'s rate limit exceeded. \n")
+                            time.sleep(60)
+                            continue
+                        except prawcore.exceptions.PRAWException as e:
+                            print(f"An error occurred while expanding comments \n")
+                            continue
 
-                    parsed_comments = [{'comment_body': comment.body} for comment in post.comments.list()]
+                        parsed_comments = [{'comment_body': comment.body} for comment in post.comments.list()]
 
-                    post_data = {  #appends variables to array
-                        'self_text': post.selftext,
-                        'title': post.title,
-                        'id': post.id,
-                        'score': post.score,
-                        'url': post.url,
-                        'permalink': post.permalink,
-                        'top_comments': parsed_comments,
-                        'linked_html_titles': linked_html_titles
-                    }
+                        post_data = {  #appends variables to array
+                            'self_text': post.selftext,
+                            'title': post.title,
+                            'id': post.id,
+                            'score': post.score,
+                            'url': post.url,
+                            'permalink': post.permalink,
+                            'top_comments': parsed_comments,
+                            'linked_html_titles': linked_html_titles
+                        }
 
-                    if not first_post:
-                        json_file.write(',')
-                    else:
-                        first_post = False
+                        if not first_post:
+                            json_file.write(',')
+                        else:
+                            first_post = False
 
-                    json.dump(post_data, json_file, ensure_ascii=False, indent=2)
+                        json.dump(post_data, json_file, ensure_ascii=False, indent=2)
 
-                    time.sleep(1)  #Used to avoid hitting rate limits
+                        time.sleep(1)  #Used to avoid hitting rate limits
 
-        json_file.write(']')
-        print(f"Data collection for {subreddit} completed \n")
+            json_file.write(']')
+            print(f"Data collection for {subreddit} completed \n")
 
-    save_seen_ids(seen_ids)
-    print("-------------------------------------------- \n")
+        save_seen_ids(seen_ids)
+        print("-------------------------------------------- \n")
+#first call to crawl and store
+crawl_and_store()
+
+#scheduler loop
+schedule.every(3).hours.do(crawl_and_store)
+
+ #Run scheduler loop
+while True:
+    schedule.run_pending()
+    time.sleep(1)
